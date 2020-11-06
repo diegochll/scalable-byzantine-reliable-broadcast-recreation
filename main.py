7954eaecbaa8b3310ddb10a8e08e3a72a38caf98
diego chamorro <diegochll@gmail.com>
2020-11-05 20:10:28 -0500

    updated starter code

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ nodes = [Node() for i in range(NODE_AMOUNT)]
 
 
 
-def handle_messages(node_number, nodes, message_queues):
-    print("I am : " + str(node_number) + " this is my node object :" + str(nodes[node_number]) + "and this is my message queue : " + str(message_queues[node_number]) )
+def handle_messages(node_number, node, message_queues):
+    print("I am : " + str(node_number) + " this is my node object :" + str(node) + "and this is my message queue : " + str(message_queues[node_number]) )
 
 
 for i in range(NODE_AMOUNT):
-    node_worker = threading.Thread(target=handle_messages, args=(i, nodes, message_queues))
+    node_worker = threading.Thread(target=handle_messages, args=(i, nodes[i], message_queues))
     node_worker.start()
 
 
