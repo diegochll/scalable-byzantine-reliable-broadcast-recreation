62a4f296f35f77822efcbff38938ee45f00caa2a
Sravya Yandamuri <spy4@duke.edu>
2020-11-06 17:28:56 -0500

    fixed random functions

--- node.py
+++ node.py
@@ -1,15 +1,14 @@
-from scipy.stats import poisson
+from numpy import random
 
 def get_random_sample(expected_sample_size,num_nodes,node_id):
-    #todo: implement Omega from paper
-    sample_size = poisson(expected_sample_size)
+    sample_size = random.poisson(expected_sample_size)
     while(sample_size<=0 or sample_size>=(num_nodes-1)): #can pick at most num_nodes-2 neighbors
-        sample_size = poisson(expected_sample_size)
+        sample_size = random.poisson(expected_sample_size)
     sample = []
     for x in range(sample_size):
-        randIndex = randint(num_nodes)
+        randIndex = random.randint(num_nodes)
         while(randIndex == node_id or randIndex in sample):
-            randIndex = randint(num_nodes)
+            randIndex = random.randint(num_nodes)
         sample.append(randIndex)
     return sample
 
