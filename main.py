d226576c8903094125618f470eba59150414f1c2
David <david.fischer@duke.edu>
2020-11-07 17:01:56 -0500

    finished murmur logic

--- main.py
+++ main.py
@@ -4,37 +4,111 @@ import threading
 
 EXPECTED_SAMPLE_SIZE = 5 # todo: figure out how to tune G according to NODE_AMOUNT
 
+CORRECT_MESSAGE = "CORRECT_MESSAGE"
+
 message_queues = [[] for i in range(NODE_AMOUNT)]
+message_queue_lock = threading.Lock()
 nodes = [Node(i,EXPECTED_SAMPLE_SIZE,NODE_AMOUNT,message_queues) for i in range(NODE_AMOUNT)]
 
 messages_delivered = []
 
-nodes[0].is_originator = True
+num_messages_delivered = 0
 
+old_num_messages_in_queues = 0
 
-def wake_up_nodes(node_list):
+debug = True
+
+"""
+if a node's message queue is nonempty, sets that node's event bool to true - this is how nodes will be reawoken after blocking
+:return false iff there are no more messages in any queue
+"""
+def wake_up_nodes():
+    global debug
+    global message_queues
+    global nodes
+    if debug:
+        print("waking up nodes...")
     to_ret = True
     for i,queue in enumerate(message_queues):
         if not len(queue) == 0:
-            node_list[i].event.set()
+            if debug:
+                print("\twaking up node {}".format(i))
+            nodes[i].event.set()
             to_ret = False
+    if debug:
+        print("done waking up nodes. returning {}".format(to_ret))
+    wake_all_nodes()
     return to_ret
 
 
-def handle_messages(node_number, node, message_queues):
-    print("I am : " + str(node_number) + " this is my node object :" + str(node) + "and this is my message queue : " + str(message_queues[node_number]) )
+def wake_all_nodes():
+    global nodes
+    for node in nodes:
+        node.event.set()
 
-    while not wake_up_nodes(nodes):
+
+"""
+while there are messages left in any queue, there is some possibility that a node will have a message delivered to it
+if a node is unable to process some message or has no more messages, it will block until its event is set
+"""
+def handle_messages(node_number, node):
+    global message_queues
+    global message_queue_lock
+    print("I am node: " + str(node_number) + "; this is my message queue: " + str([str(message) for message in message_queues[node_number]]))
+    print("\tthis is my gossip set: {}".format(node.G))
+    global debug
+    # the first node is the originator and needs to send out the message to decide upon
+    if node_number == 0:
+        print("I am node 0 and the originator; broadcasting gossip with correct value...")
+        node.is_originator = True
+        message_queue_lock.acquire(True)
+        node.broadcast("GOSSIP",CORRECT_MESSAGE,message_queues)
+        message_queue_lock.release()
+
+    message_queue_lock.acquire(True)
+    while not wake_up_nodes():
+        if debug:
+            print("there are messages left in the message queues; node {} is processing incoming messages...".format(node_number))
+            print("\tnode {}'s message list: {}".format(node_number,str([str(message) for message in message_queues[node_number]])))
         if not node.receive(message_queues):
+            if debug:
+                print("node {} could not receive a message. blocking until it is woken...".format(node_number))
             # there are no more messages for this node (or some sort of failure on receiving a message); we want to yield to the next node
+            message_queue_lock.release()
+            node.event.clear()
             node.event.wait()
-
+            message_queue_lock.acquire(True)
+    print("message list is empty.")
+    message_queue_lock.release()
+    return
 
 
 
 def main():
+    print("starting simulation.")
+    global message_queues
+    global nodes
+
+    node_workers = []
     for i in range(NODE_AMOUNT):
-        node_worker = threading.Thread(target=handle_messages, args=(i, nodes[i], message_queues))
+        node_worker = threading.Thread(target=handle_messages, args=(i, nodes[i]))
         node_worker.start()
+        node_workers.append(node_worker)
+
+    for worker in node_workers:
+        worker.join()
+
+    print("finished simulation.")
+    print("counting number of nodes which delivered the correct message...")
+    num_correct_delivery = 0
+    num_messages_delivered = 0
+    for node in nodes:
+        if node.delivered.type != "DEFAULT" and node.delivered.content == CORRECT_MESSAGE:
+            num_correct_delivery += 1
+            num_messages_delivered += node.num_messages_sent
+    print("number of nodes which delivered the correct message: {}".format(num_correct_delivery))
+    print("total messages sent: {}".format(num_messages_delivered))
 
 
+if __name__ == "__main__":
+    main()
--- node.py
+++ node.py
@@ -18,52 +18,66 @@ def get_random_sample(expected_sample_size,num_nodes,node_id):
 
 class Message:
     def __init__(self,originator,message_type,content,signature = ""):
+        #todo: implement message signatures
         self.originator = originator
         self.type = message_type
         self.content = content
         self.signature = signature
 
+    def __str__(self):
+        return "Message(from: '{}'; type: '{}'; content: '{}'; signature: '{}')".format(self.originator,self.type, self.content,self.signature)
+
 class Node:
     def __init__(self,node_id,expected_sample_size,num_nodes,message_queues):
         self.node_id = node_id
         self.G = set(get_random_sample(expected_sample_size,num_nodes, self.node_id))
-        for g in self.G:
-            self.send(g,"GOSSIP_SUBSCRIBE","",message_queues)
+        self.num_messages_sent = 0
         self.is_originator = False
-        self.delivered = Message(-1,"PLACE_HOLDER","")
+        self.delivered = Message(-1, "DEFAULT", "")
         self.event = threading.Event()
+        for g in self.G:
+            m = Message(self.node_id,"GOSSIP_SUBSCRIBE","default")
+            self.send(g,m,message_queues)
 
     def broadcast(self,type,message,node_message_lists):
         # only used by originator
-        if not self.is_originator:
-            return
+        if self.is_originator:
+            m = Message(self.node_id,type,message)
+            print("\toriginator in broadcast function; broadcasting message {}. calling dispatch...".format(str(m)))
+            self.dispatch(m,node_message_lists)
 
-        for message_list in node_message_lists:
-            message_list.append(Message(self.node_id,type,message))
-
-    def send(self,node,type,content,node_message_lists):
-        node_message_lists[node].append(Message(self.node_id,type,content))
+    def send(self,node,message,node_message_lists):
+        self.num_messages_sent += 1
+        print("node {} appending message {} to node {}'s message list, {}".format(self.node_id,str(message),node,str([str(message) for message in node_message_lists[node]])))
+        node_message_lists[node].append(message)
+        print("\t node {}'s message list after sending the message: {}".format(node,str([str(message) for message in node_message_lists[node]])))
 
     def dispatch(self,message,node_message_lists):
-        if self.delivered.type == "PLACE_HOLDER":
+        print("this node's 'delivered' message is {}; attempting to dispatch message {}...".format(str(self.delivered),str(message)))
+        if self.delivered.type == "DEFAULT": # no message has been delivered yet
+            print("\tnode {}'s delivered type is default, and outgoing message's type is not default...".format(self.node_id))
             self.delivered = message
-
+            print("\tsending message to nodes: {}".format(self.G))
             for node in self.G:
-                self.send(node,"GOSSIP",message.content,node_message_lists)
+                self.send(node,message,node_message_lists)
 
     def receive(self,node_message_lists):
-        if node_message_lists[self.node_id].empty():
+        if len(node_message_lists[self.node_id]) == 0:
             return False
         message = node_message_lists[self.node_id][0]
-        node_message_lists[self.node_id].pop()
+        node_message_lists[self.node_id].pop(0)
+        print("node {} receiving message {}".format(self.node_id,str(message)))
         if message.type == "GOSSIP_SUBSCRIBE":
-            if not self.delivered.content == "":
-                message_originator = message.originator
-                node_message_lists[message_originator].append(Message(self.node_id,"GOSSIP",self.delivered))
+            print("\t node {} receiving a gossip subscription from node {}. adding to gossip set...".format(self.node_id,message.originator))
+            if not self.delivered.type == "DEFAULT":
+                # self already delivered a value, so send it along to the node requesting a gossip subscription
+                m = Message(self.node_id,self.delivered.type,self.delivered.content,self.delivered.signature)
+                self.send(message.originator,m,node_message_lists)
             self.G.add(message.originator)
             return True
 
         elif message.type == "GOSSIP":
+            print("\tnode {} receiving gossip from node {}; dispatching...".format(self.node_id,message.originator))
             if self.verify(message):
                 self.dispatch(message,node_message_lists)
             return True
